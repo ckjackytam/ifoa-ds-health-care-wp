1de43c2e66cd2c655af0f4508ae86dd83d3191fa
Luteijn Michiel <Michiel.Luteijn@hannover-re.com>
2025-12-16 16:52:54 +0000

    read data and save parquet helper

---
+++ R/Helpers/read_data_and_save_parquet.R
@@ -0,0 +1,19 @@
+
+start <- Sys.time()
+ilec <- read_delim("Data/ILEC_2012_19 - 20240429.txt", 
+                   delim = "\t", escape_double = FALSE, 
+                   trim_ws = TRUE) %>% data.table
+Sys.time() - start # 3 minutes
+
+drop_columns <- grep("^(ExpDth_|Cen)", names(ilec), value = TRUE)
+ilec[, (drop_columns) := NULL]
+
+# retain TERM business
+ilec <- ilec[Insurance_Plan == "Term"]
+
+int_cols <- c("Attained_Age", "Issue_Age", "Duration", "Issue_Year", 
+              "Preferred_Class", "Number_of_Pfd_Classes", "Observation_Year")
+ilec[, (int_cols) := lapply(.SD, as.integer), .SDcols = int_cols]
+
+arrow::write_parquet(ilec, "Data/ILEC_2012_19 - 20240429.parquet")
+
