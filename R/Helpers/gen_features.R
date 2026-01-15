b5cc527eb2fa826331ac4341292df423c366096a
Luteijn Michiel <Michiel.Luteijn@hannover-re.com>
2026-01-15 13:17:54 +0000

    Added orthogonal polynomials to observation year

--- R/Helpers/gen_features.R
+++ R/Helpers/gen_features.R
@@ -1,4 +1,4 @@
-gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL){
+gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL, obs_year_coefs = NULL){
   
   agepoly <- if (!is.null(age_coefs)) {
     poly(dataset$Attained_Age, degree = 3, coefs = age_coefs) %>% data.table
@@ -10,10 +10,16 @@ gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL){
   } else {
     poly(dataset$Issue_Age, degree = 3) %>% data.table
   }
+  obs_yearpoly <- if (!is.null(obs_year_coefs)) {
+    poly(dataset$Observation_Year, degree = 3, coefs = obs_year_coefs) %>% data.table
+  } else {
+    poly(dataset$Observation_Year, degree = 3) %>% data.table
+  }
   agesplines <- ns(dataset$Attained_Age, knots = c(20, 30, 40, 50), Boundary.knots = c(18, 90)) %>% data.table
   
   names(agepoly) <- paste0("agepoly", seq(1,3,1))
-  names(entry_agepoly) <- paste0("Issue_Agepoly", seq(1,3,1))
+  names(entry_agepoly) <- paste0("issue_agepoly", seq(1,3,1))
+  names(obs_yearpoly) <- paste0("obs_yearpoly", seq(1,3,1))
   names(agesplines) <- paste0("agespline", seq(1,5,1))
   
   dataset[, ':=' (dur0 = fifelse(Duration == 1, 1, 0),
@@ -31,7 +37,7 @@ gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL){
   )]
   
   dataset <- cbind(
-    dataset, agepoly, entry_agepoly, agesplines
+    dataset, agepoly, entry_agepoly, obs_yearpoly, agesplines
   ) %>% data.table
   
   if("Observation_Year" %in% names(dataset)){
