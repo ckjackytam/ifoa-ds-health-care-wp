813c4663325c0f18a797952cb0a345c3831d04d8
Luteijn Michiel <Michiel.Luteijn@hannover-re.com>
2026-01-15 13:18:18 +0000

    AtoE plots now properly handly x-axis of integers such as year

--- R/Helpers/AE_plotting_functions.R
+++ R/Helpers/AE_plotting_functions.R
@@ -19,7 +19,30 @@ ae_summary_by <- function(dt, by_col, actual = actual_col, pred = pred_col, conf
   ans[]
 }
 
-ae_plot <- function(summ_dt, by_col, top_n = 50) {
+
+ae_plot <- function(summ_dt, by_col, top_n = 50, max_integer_breaks = 25) {
+  is_integer_like <- function(x) {
+    if (!is.numeric(x)) return(FALSE)
+    all(is.na(x) | (abs(x - round(x)) < .Machine$double.eps^0.5))
+  }
+  
+  build_integer_x_scale <- function(x_vals, max_breaks = 25) {
+    ux <- sort(unique(na.omit(as.numeric(x_vals))))
+    if (length(ux) <= max_breaks) {
+      ggplot2::scale_x_continuous(
+        breaks = ux,
+        labels = scales::number_format(accuracy = 1, big.mark = ""),
+        expand = ggplot2::expansion(mult = c(0.01, 0.05))
+      )
+    } else {
+      ggplot2::scale_x_continuous(
+        breaks = scales::breaks_pretty(n = 8)(ux),
+        labels = scales::number_format(accuracy = 1, big.mark = ""),
+        expand = ggplot2::expansion(mult = c(0.01, 0.05))
+      )
+    }
+  }
+  
   plot_dt <- data.table::copy(summ_dt)
   orig_col    <- summ_dt[[by_col]]
   is_num      <- is.numeric(orig_col)
@@ -30,21 +53,35 @@ ae_plot <- function(summ_dt, by_col, top_n = 50) {
     plot_dt <- plot_dt[order(get(by_col))]
     x_var <- plot_dt[[by_col]]
     title_suffix <- ""
+    
+    unique_count <- length(unique(na.omit(x_var)))
+    treat_as_discrete <- is_integer_like(x_var) && unique_count <= max_integer_breaks
+    x_scale <- if (treat_as_discrete) {
+      plot_dt[, (by_col) := factor(get(by_col), levels = unique(plot_dt[[by_col]]))]
+      x_var <- plot_dt[[by_col]]
+      ggplot2::scale_x_discrete(drop = FALSE)
+    } else {
+      build_integer_x_scale(x_var, max_integer_breaks)
+    }
+    
   } else if (is_ord_fact) {
     levs <- levels(orig_col)
     plot_dt[, (by_col) := factor(as.character(get(by_col)),
                                  levels = levs, ordered = TRUE)]
-    plot_dt[, ..ord_idx := as.integer(get(by_col))]
-    plot_dt <- plot_dt[order(..ord_idx)]
-    plot_dt[, ..ord_idx := NULL]
+    plot_dt[, `.ord_idx` := as.integer(get(by_col))]
+    plot_dt <- plot_dt[order(.ord_idx)]
+    plot_dt[, `.ord_idx` := NULL]
     x_var <- plot_dt[[by_col]]
     title_suffix <- ""
+    x_scale <- ggplot2::scale_x_discrete(drop = FALSE)
+    
   } else {
     plot_dt <- plot_dt[order(-pred)]
     if (!is.null(top_n) && nrow(plot_dt) > top_n) plot_dt <- plot_dt[1:top_n]
     plot_dt[, (by_col) := factor(get(by_col), levels = plot_dt[order(ae)][[by_col]])]
     x_var <- plot_dt[[by_col]]
     title_suffix <- sprintf(" (top %s levels by exposure)", ifelse(is.null(top_n), "all", top_n))
+    x_scale <- ggplot2::scale_x_discrete(drop = FALSE)
   }
   
   eps <- .Machine$double.eps
@@ -58,13 +95,14 @@ ae_plot <- function(summ_dt, by_col, top_n = 50) {
   hjust <- if (angle == 45) 1 else 0.5
   vjust <- if (angle == 45) 1 else 0.5
   
-  ggplot2::ggplot(plot_dt, ggplot2::aes(x = x_var, y = ae)) +
+  p <- ggplot2::ggplot(plot_dt, ggplot2::aes(x = x_var, y = ae)) +
     ggplot2::geom_hline(yintercept = 1, colour = "grey50", linetype = "dashed") +
     ggplot2::geom_point(size = 2, colour = "#1f77b4") +
     ggplot2::geom_errorbar(ggplot2::aes(ymin = lower_pos, ymax = upper_pos),
                            width = 0.15, colour = "#1f77b4", alpha = 0.7) +
     ggplot2::scale_y_log10("Actual / Predicted (log scale)",
                            limits = c(y_min, y_max)) +
+    x_scale +
     ggplot2::xlab(by_col) +
     ggplot2::ggtitle(sprintf("A/E by %s%s", by_col, title_suffix)) +
     ggplot2::theme_minimal(base_size = 12) +
@@ -72,4 +110,7 @@ ae_plot <- function(summ_dt, by_col, top_n = 50) {
       axis.text.x = ggplot2::element_text(angle = angle, hjust = hjust, vjust = vjust),
       plot.title  = ggplot2::element_text(face = "bold")
     )
+  
+  return(p)
 }
+
