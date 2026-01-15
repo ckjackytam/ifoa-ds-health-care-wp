4e894d1fe788055163f377597b65f88a226c4982
Luteijn Michiel <Michiel.Luteijn@hannover-re.com>
2026-01-15 11:56:46 +0000

    Updated gen_features to ensure orthogonal polynomials use same coefs on test dataset

--- R/Helpers/gen_features.R
+++ R/Helpers/gen_features.R
@@ -1,6 +1,15 @@
-gen_features <- function(dataset){
-  agepoly <- poly(dataset$Attained_Age, 3) %>% data.table
-  entry_agepoly <- poly(dataset$Issue_Age, 3) %>% data.table
+gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL){
+  
+  agepoly <- if (!is.null(age_coefs)) {
+    poly(dataset$Attained_Age, degree = 3, coefs = age_coefs) %>% data.table
+  } else {
+    poly(dataset$Attained_Age, degree = 3) %>% data.table
+  }
+  entry_agepoly <- if (!is.null(entry_age_coefs)) {
+    poly(dataset$Issue_Age, degree = 3, coefs = entry_age_coefs) %>% data.table
+  } else {
+    poly(dataset$Issue_Age, degree = 3) %>% data.table
+  }
   agesplines <- ns(dataset$Attained_Age, knots = c(20, 30, 40, 50), Boundary.knots = c(18, 90)) %>% data.table
   
   names(agepoly) <- paste0("agepoly", seq(1,3,1))
