2e8bb3b1fc01001b8f9294460bba8b665b6f3aa1
Luteijn Michiel <Michiel.Luteijn@hannover-re.com>
2026-01-24 22:11:33 +0000

    Added orthogonal polynomials for issue year.

--- R/Helpers/gen_features.R
+++ R/Helpers/gen_features.R
@@ -1,4 +1,4 @@
-gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL, obs_year_coefs = NULL){
+gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL, obs_year_coefs = NULL, issue_year_coefs = NULL){
   
   agepoly <- if (!is.null(age_coefs)) {
     poly(dataset$Attained_Age, degree = 3, coefs = age_coefs) %>% data.table
@@ -15,11 +15,17 @@ gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL, obs_
   } else {
     poly(dataset$Observation_Year, degree = 3) %>% data.table
   }
+  issue_yearpoly <- if (!is.null(issue_year_coefs)) {
+    poly(dataset$Issue_Year, degree = 3, coefs = issue_year_coefs) %>% data.table
+  } else {
+    poly(dataset$Issue_Year, degree = 3) %>% data.table
+  }
   agesplines <- ns(dataset$Attained_Age, knots = c(20, 30, 40, 50), Boundary.knots = c(18, 90)) %>% data.table
   
   names(agepoly) <- paste0("agepoly", seq(1,3,1))
   names(entry_agepoly) <- paste0("issue_agepoly", seq(1,3,1))
   names(obs_yearpoly) <- paste0("obs_yearpoly", seq(1,3,1))
+  names(issue_yearpoly) <- paste0("issue_yearpoly", seq(1,3,1))
   names(agesplines) <- paste0("agespline", seq(1,5,1))
   
   dataset[, ':=' (dur0 = fifelse(Duration == 1, 1, 0),
@@ -37,23 +43,12 @@ gen_features <- function(dataset, age_coefs = NULL, entry_age_coefs = NULL, obs_
   )]
   
   dataset <- cbind(
-    dataset, agepoly, entry_agepoly, obs_yearpoly, agesplines
+    dataset, agepoly, entry_agepoly, obs_yearpoly, issue_yearpoly, agesplines
   ) %>% data.table
   
   if("Observation_Year" %in% names(dataset)){
     dataset[, ':=' (Observation_YearChar = as.character(Observation_Year)
     )]
-  }
-  
-  if("Issue_Year" %in% names(dataset)){
-    
-    min_uwYear <- min(dataset$Issue_Year)
-    max_uwYear <- max(dataset$Issue_Year)
-    dataset[, ':=' (uw_year_01 = (Issue_Year - min_uwYear) / (max_uwYear - min_uwYear))]
-    
-    dataset[, ':=' (uwYearChar = as.character(Issue_Year),
-                    uw_year_01_sq = uw_year_01 ^2,
-                    uw_year_01_cube = uw_year_01 ^3)]
   }
   
   return(dataset)}
